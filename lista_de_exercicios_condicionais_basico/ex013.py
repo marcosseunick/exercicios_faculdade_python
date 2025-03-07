9d6eeb9c63832e3bd1f31d181bf0400d06e44030
marcosseunick <mv7989666@gmail.com>
2025-03-07 09:41:44 -0300

    Create ex013.py

---
+++ lista_de_exercicios_condicionais_basico/ex013.py
@@ -0,0 +1,18 @@
+# 13 - Desenvolva um algoritmo que receba do usuário os  valores a, b e c, de uma equação do segundo grau  (ax^2 + bx + c = 0) e determine as raízes.
+import math
+
+a = float(input("Digite o coeficiente a: "))
+b = float(input("Digite o coeficiente b: "))
+c = float(input("Digite o coeficiente c: "))
+
+delta = b ** 2 - 4 * a * c
+
+if delta > 0:
+  x1 = (-b + math.sqrt(delta)) / (2*a)
+  x2 = (-b - math.sqrt(delta)) / (2*a)
+  print(f"As raizes da equação são: x1 = {x1:.2f} e x2 = {x2:.2f}")
+elif delta == 0:
+  x = -b / (2*a)
+  print(f"A equação tem uma raiz única real: x = {x:.2f}")
+else:
+  print("A equação não possuí raízes reais")
