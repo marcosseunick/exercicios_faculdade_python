1a9fae8deae1a49f55c1df1fe1998b5967005051
marcosseunick <mv7989666@gmail.com>
2025-03-12 17:38:37 -0300

    Update ex015.py

---
+++ lista_de_exercicios_condicionais_basico/ex015.py
@@ -0,0 +1,3 @@
+# Crie um programa dado solicite a largura e o  comprimento de uma sala (supondo que a sala seja quadrática), 
+# solicite a largura e o tamanho do piso  e mostre quantos peças do preciso será necessário para revestir toda a sala
+
