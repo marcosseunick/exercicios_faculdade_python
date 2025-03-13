cd0008cb3e88d72ed9c2df037e747b92b0212e0c
marcosseunick <mv7989666@gmail.com>
2025-03-13 16:04:43 -0300

    Update ex015.py

--- lista_de_exercicios_condicionais_basico/ex015.py
+++ lista_de_exercicios_condicionais_basico/ex015.py
@@ -1,10 +1,20 @@
 # Crie um programa dado solicite a largura e o  comprimento de uma sala (supondo que a sala seja quadrática), 
 # solicite a largura e o tamanho do piso  e mostre quantos peças do preciso será necessário para revestir toda a sala
 
-largura = float(input("Digite a largura da sala: "))
-comprimento = float(input("Digite o comprimento da sala: "))
-largura_do_piso = float(input("Digite a largura do seu piso: "))
-tamanho_piso = float(input("Digite o tamanho do seu piso: "))
+def validar_entrada(mensagem):
+  while True:
+    try:
+      valor = float(input(mensagem))
+      if valor > 0:
+        return valor
+      print("Valor Inválido. Digite um Número maior que 0")
+    except ValueError:
+      print("Entrada Inválida. Digite um número válido")
+
+largura = validar_entrada("Digite a largura da sala em metros: ")
+comprimento = validar_entrada("Digite o comprimento da sala em metros: ")
+largura_do_piso = validar_entrada("Digite a largura do seu piso em metros: ")
+tamanho_piso = validar_entrada("Digite o tamanho do seu piso em metros: ")
 
 area_sala = largura * comprimento
 area_piso = largura * tamanho_piso
