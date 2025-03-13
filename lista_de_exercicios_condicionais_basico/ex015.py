ec61304f001661dce3046629cae8e4d5b0f42405
marcosseunick <mv7989666@gmail.com>
2025-03-13 15:54:33 -0300

    Update ex015.py

--- lista_de_exercicios_condicionais_basico/ex015.py
+++ lista_de_exercicios_condicionais_basico/ex015.py
@@ -1,3 +1,13 @@
 # Crie um programa dado solicite a largura e o  comprimento de uma sala (supondo que a sala seja quadrática), 
 # solicite a largura e o tamanho do piso  e mostre quantos peças do preciso será necessário para revestir toda a sala
 
+largura = float(input("Digite a largura da sala: "))
+comprimento = float(input("Digite o comprimento da sala: "))
+largura_do_piso = float(input("Digite a largura do seu piso: "))
+tamanho_piso = float(input("Digite o tamanho do seu piso: "))
+
+area_sala = largura * comprimento
+area_piso = largura * tamanho_piso
+quantidade_de_pecas = area_sala/area_piso
+
+print(f"Você usará aproximadamente {quantidade_de_pecas} pisos")
