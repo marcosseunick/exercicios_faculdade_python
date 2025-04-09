30e531e23f45b2e469d896e4bd273866c8a2df5c
Marcos Ferreira <mv7989666@gmail.com>
2025-04-09 09:55:46 -0300

    Create ex003.py

---
+++ lista_de_exercícios_python_4.1/ex003.py
@@ -0,0 +1,6 @@
+#3. Faça um algoritmo que receba uma frase converta cada palavra em hashtags para redes sociais, sendo que:
+#  a. Deve começar com uma hashtag (#).
+#  b. Todas as palavras devem ter a primeira letra maiúscula.
+#  c. Apenas palavras a partir de 05 caracteres
+#  Por exemplo, se a entrada for a frase: a mancha verde é o poder, a saída deve ser: #Mancha #Verde #Poder
+
