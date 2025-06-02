4b411b440f9b0702a34a8f0c6cce0d01713c95cf
Marcos Ferreira <mv7989666@gmail.com>
2025-06-02 17:47:56 -0300

    Create funcoes_com_dicionarios.py

---
+++ atividade_em_dupla/funcoes_com_dicionarios.py
@@ -0,0 +1,18 @@
+#Funções com dicionários:
+
+#1
+# def obter_valor(dicionario, chave): → Retorne o valor associado a uma chave em um dicionário. Se a chave não existir, retorne None.
+
+# def chave_existe(dicionario, chave): → Verifique se uma determinada chave existe no dicionário.
+
+# def adicionar_par(dicionario, chave, valor): → Adicione ou atualize um par chave-valor em um dicionário e retorne o dicionário atualizado.
+
+# def remover_chave(dicionario, chave): → Remova uma chave do dicionário, se ela existir, e retorne o novo dicionário.
+
+# def contar_chaves(dicionario): → Retorne a quantidade de chaves existentes no dicionário.
+
+# def inverter_dicionario(dicionario): → Retorne um novo dicionário com as chaves e valores invertidos.
+
+# def mesclar_dicionarios(d1, d2): → Retorne um novo dicionário contendo os pares de d1 e d2, priorizando os valores de d2 em caso de conflito.
+
+# def contar_ocorrencias(lista): → Receba uma lista e retorne um dicionário com a contagem de ocorrências de cada elemento.
