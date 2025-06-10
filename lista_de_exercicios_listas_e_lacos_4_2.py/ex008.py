544c33c1848531bf7062f46a481e5fe6b8067c9e
Marcos Ferreira <mv7989666@gmail.com>
2025-06-10 13:50:36 -0300

    Create ex008.py

---
+++ lista_de_exercicios_listas_e_lacos_4_2.py/ex008.py
@@ -0,0 +1,12 @@
+# Dada uma sequência de letras minúsculas ('a' - 'z'),
+# obtenha a distância máxima entre duas letras iguais
+# e retorne essa distância junto com a letra que a
+# formou.
+# Caso exista mais de uma letra com a mesma
+# distância máxima, retorne a que aparece primeiro.
+# Exemplo:
+# Para "fffffahhhhhhaaahhhhbhhahhhhabxx", a
+# saída deve ser "a23".
+# A distância máxima é formada pelo caracter 'a' de
+# índice 5 ao índice 27 (baseado em 0). Portanto, a
+# saída é "a23".
