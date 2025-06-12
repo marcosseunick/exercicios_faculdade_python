e2eddeee71b3bd4092f88753d0e048db9d63e642
Marcos Ferreira <mv7989666@gmail.com>
2025-06-11 21:19:16 -0300

    Update ex008.py

--- lista_de_exercicios_listas_e_lacos_4_2.py/ex008.py
+++ lista_de_exercicios_listas_e_lacos_4_2.py/ex008.py
@@ -1,16 +1,3 @@
-# Dada uma sequência de letras minúsculas ('a' - 'z'),
-# obtenha a distância máxima entre duas letras iguais
-# e retorne essa distância junto com a letra que a
-# formou.
-# Caso exista mais de uma letra com a mesma
-# distância máxima, retorne a que aparece primeiro.
-# Exemplo:
-# Para "fffffahhhhhhaaahhhhbhhahhhhabxx", a
-# saída deve ser "a23".
-# A distância máxima é formada pelo caracter 'a' de
-# índice 5 ao índice 27 (baseado em 0). Portanto, a
-# saída é "a23".
-
 def distancia_maxima_caractere(sequencia):
     primeira_ocorrencia = {}
     ultima_ocorrencia = {}
@@ -23,7 +10,7 @@ def distancia_maxima_caractere(sequencia):
     distancia_maxima = -1
     caractere_maxima = ''
     primeiro_indice_caractere_maxima = float('inf')
-    
+
     for char in primeira_ocorrencia:
         distancia = ultima_ocorrencia[char] - primeira_ocorrencia[char]
 
