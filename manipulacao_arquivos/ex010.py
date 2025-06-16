455158bd5f6a2bd36610801259f4025792259ffe
Marcos Ferreira <mv7989666@gmail.com>
2025-06-16 20:55:52 -0300

    Create ex010.py

---
+++ manipulacao_arquivos/ex010.py
@@ -0,0 +1,5 @@
+# Mostrar Linhas com Palavra-Chave
+# Crie uma função linhas_com_palavra(nome_arquivo, palavra) que retorne uma lista com as linhas do arquivo que contêm a palavra informada.
+
+def linhas_com_palavra(nome_arquivo, palavra):
+    
