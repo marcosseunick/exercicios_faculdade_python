d4c74614bd70f929f87181d224efce5eb21495ba
Marcos Ferreira <mv7989666@gmail.com>
2025-06-16 20:54:27 -0300

    Create exercicios.py

---
+++ manipulacao_arquivos/estudos da prova/exercicios.py
@@ -0,0 +1,35 @@
+# Criação e Acesso: Crie uma lista chamada frutas com pelo menos 5 frutas diferentes. Acesse e imprima a terceira fruta da lista.
+
+# frutas = ["Abacate", "Mamão", "Maçã", "Abacaxi", "Limão"]
+           #0         #1        #2      #3          #4
+
+# print(frutas[2])
+
+# Adicionar e Remover: Adicione uma nova fruta ao final da lista frutas. Em seguida, remova a segunda fruta da lista. Imprima a lista modificada.
+
+# frutas.append("Laranja")
+# del frutas[1]
+# print(frutas)
+
+#Contagem e Busca: Crie uma lista de números inteiros. Peça ao usuário para digitar um 
+# número e verifique se ele está na lista. Se estiver, imprima quantas vezes ele aparece.
+
+# lista = [1, 2, 3, 4, 5, 4, 3, 5, 10, 23, 45]
+# numero = int(input("Digite um número: "))
+# if numero in lista:
+#     print(lista.count(numero))
+# else:
+#     print("Não está na lista")
+
+#Ordenação e Inversão: Crie uma lista de nomes. Ordene a lista em ordem alfabética e depois imprima a lista invertida.
+
+# nomes = ["Marcos", "Isabela", "Rafael", "Snoopy", "Mario"]
+# nomes.sort()
+# nomes.reverse()
+# print(nomes)
+
+#Conversão Lista-Tupla: Crie uma lista de números. Converta essa lista em uma tupla e imprima a tupla resultante.
+# lista = [1, 3, 2, 4, 4, 5, 7, 3, 2, 4, 56, 8, 8, 5]
+# tupla = tuple(lista)
+# print(tupla)
+
