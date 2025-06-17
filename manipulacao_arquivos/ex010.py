5e5a42c94c8885f3c50981c090ebf7821b221e4d
marcosseunick <mv7989666@gmail.com>
2025-06-16 21:34:22 -0300

    Update ex010.py

--- manipulacao_arquivos/ex010.py
+++ manipulacao_arquivos/ex010.py
@@ -2,4 +2,10 @@
 # Crie uma função linhas_com_palavra(nome_arquivo, palavra) que retorne uma lista com as linhas do arquivo que contêm a palavra informada.
 
 def linhas_com_palavra(nome_arquivo, palavra):
+  with open(nome_arquivo, "r", encoding="UTF-8") as arquivo:
+            linhas_encontradas = []
+            for linha in arquivo:
+                if palavra in linha:
+                    linhas_encontradas.append(linha.strip())
+                return linhas_encontradas
     
