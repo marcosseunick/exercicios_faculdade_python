de88d2d2b91598d2b8fc048289a133b5ffc4b46e
Marcos Ferreira <mv7989666@gmail.com>
2025-10-06 12:42:17 -0300

    summary

--- academia-projeto/lib/logica_exercicios.py
+++ academia-projeto/lib/logica_exercicios.py
@@ -3,8 +3,9 @@ import gerenciador_json
 ARQUIVO_EXERCICIOS = "db/exercicios.json"
 
 def listar_exercicios():
-    dados = gerenciador_json.carregar_dados(ARQUIVO_EXERCICIOS)
-    return dados
+    dados = gerenciador_json.carregar_exercicios(ARQUIVO_EXERCICIOS)
+    print(dados)
+
 
 def adicionar_exercicio():
     pass
@@ -17,3 +18,5 @@ def remover_exercicio(id_do_exercicio):
 
 def atualizar_exercicio(id_do_exercicio, novos_dados):
     pass
+
+
