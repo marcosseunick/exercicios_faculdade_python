bc30851ec84294f13ba24498d4862d9587856033
Marcos Ferreira <mv7989666@gmail.com>
2025-10-08 08:28:29 -0300

    Update gerenciador_json.py

--- academia-projeto/lib/gerenciador_json.py
+++ academia-projeto/lib/gerenciador_json.py
@@ -18,18 +18,4 @@ def salvar_dados_do_arquivo(caminho_do_arquivo, dados_para_salvar):
         print(f"Erro ao salvar o aquivo {caminho_do_arquivo} : {e}")
         return False
     
-    
-CAMINHO_EXERCICIOS = 'db/exercicios.json'
-CAMINHO_TREINOS = 'db/treinos.json'
 
-def carregar_exercicios(caminho_do_arquivo):
-    return carregar_dados_do_arquivo(CAMINHO_EXERCICIOS)
-
-def salvar_exercicios(lista_exercicios):
-    return salvar_dados_do_arquivo(CAMINHO_EXERCICIOS, lista_exercicios)
-
-def carregar_treinos():
-    return carregar_dados_do_arquivo(CAMINHO_TREINOS)
-
-def salvar_treinos(lista_treinos):
-    return salvar_dados_do_arquivo(CAMINHO_TREINOS, lista_treinos)
