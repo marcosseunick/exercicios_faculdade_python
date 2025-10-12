3da580c51a43db37266a8ddcc5885cbd9df330b9
sjanesilva2018-del <sjane.silva2018@gmail.com>
2025-10-11 21:55:51 -0300

    sarah está enviando a parte de adc exercicios

--- academia-projeto/lib/logica_exercicios.py
+++ academia-projeto/lib/logica_exercicios.py
@@ -1,5 +1,7 @@
 import gerenciador_json
-
+import pandas as pd
+import os
+import json
 
 ARQUIVO_EXERCICIOS = "db/exercicios.json"
 
@@ -7,8 +9,63 @@ def listar_exercicios():
     dados = gerenciador_json.carregar_dados_do_arquivo(ARQUIVO_EXERCICIOS)
     return dados
 
-def adicionar_exercicio(nome, tipo, repeticoes):
-    pass
+def adicionar_exercicio():
+    print("\n--- ADICIONAR NOVO Exercicio ---")
+    
+    pessoa = input("Digite o nome da pessoa: ")
+    idade = int(input("Digite a idade da pessoa: "))
+    nome = input("Digite o nome do novo exercicio: ")
+    tipo = input("Digite o tipo do novo exercicio: ")
+    repeticoes = int(input("Digite a quantidadade de repeticoes do novo exercicio: "))
+    df = ler_dados()
+    if not df.empty and 'id' in df.columns and pd.notna(df['id']).any():
+        novo_id = df['id'].max() + 1
+    else:
+        novo_id = 1
+
+    novo_exercicio = {'id': novo_id, 'nome': pessoa, "idade": idade,'exercicio': nome, 'tipo': tipo, 'repeticao': repeticoes}
+    df_novo_exercicio = pd.DataFrame([novo_exercicio])
+    df_atualizado = pd.concat([df, df_novo_exercicio], ignore_index=True)
+    salvar_dados(df_atualizado)
+    
+    return df_atualizado
+def ler_dados() -> pd.DataFrame:
+    CHAVE_PRINCIPAL = 'exercicios'
+    try:
+        
+        if not os.path.exists(ARQUIVO_EXERCICIOS):
+            return pd.DataFrame(columns=['id', 'nome', 'tipo', 'quantidade'], dtype=object)
+        with open(ARQUIVO_EXERCICIOS, 'r') as f:
+            dados_json = json.load(f)
+        df = pd.json_normalize(dados_json[CHAVE_PRINCIPAL])
+        if not df.empty and 'id' in df.columns:
+            df['id'] = pd.to_numeric(df['id'], errors='coerce').astype('Int64') 
+        return df
+    
+    
+    except KeyError:
+        print(f"ERRO: Chave '{CHAVE_PRINCIPAL}' não encontrada no JSON. Retornando DF vazio.")
+        return pd.DataFrame(columns=['id', 'nome', 'idade'], dtype=object)
+    
+    except Exception as e:
+        print(f"ERRO de Leitura: Não foi possível processar o JSON. Detalhes: {e}")
+        return pd.DataFrame(columns=['id', 'nome', 'idade'], dtype=object)
+
+def salvar_dados(df: pd.DataFrame):
+    CHAVE_PRINCIPAL = 'exercicios'
+    try:
+    
+        dados_lista = df.to_dict(orient='records')
+        dados_para_salvar = {
+            CHAVE_PRINCIPAL: dados_lista
+        }
+        os.makedirs(os.path.dirname(ARQUIVO_EXERCICIOS), exist_ok=True)
+        with open(ARQUIVO_EXERCICIOS, 'w') as f:
+            json.dump(dados_para_salvar, f, indent=4)
+            
+        print(f"\n[SUCESSO] Dados salvos e arquivo atualizado em {ARQUIVO_EXERCICIOS}.")
+    except Exception as e:
+        print(f"\n[ERRO] de Escrita: Não foi possível salvar o arquivo. Detalhes: {e}")
 
 def buscar_exercicio_por_nome(nome_pesquisado):
     pass
@@ -35,5 +92,3 @@ def atualizar_exercicio(id_do_exercicio, novos_dados):
             exercicio.update(novos_dados)
             break
     gerenciador_json.salvar_dados_do_arquivo(ARQUIVO_EXERCICIOS, lista_exercicios)
-
-
